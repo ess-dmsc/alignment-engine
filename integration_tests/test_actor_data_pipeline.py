03c080da0059dbf1ef07d96e671a855f7e330cb5
Jonas Petersson <jonas.petersson@ess.eu>
2023-07-19 17:47:53 +0200

    checking f144 and ev44

--- integration_tests/test_actor_data_pipeline.py
+++ integration_tests/test_actor_data_pipeline.py
@@ -12,7 +12,7 @@ from src.main.actors.data_handler_actor import DataHandlerActor, DataHandlerLogi
 
 from streaming_data_types import deserialise_ev44, deserialise_f144
 
-class TestConsumerActor:
+class TestConsumerActorf144:
     @pytest.fixture
     def setup(self):
         self.f144_data = generate_fake_f144_data(10)
@@ -43,11 +43,58 @@ class TestConsumerActor:
 
         self.consumer_actor_ref.tell('START')
 
+        assert self.consumer_actor_proxy.get_status().get() == 'RUNNING'
+
+        time.sleep(0.1)
+
+        self.consumer_actor_ref.tell('STOP')
+
+        final_data = self.data_handler_actor_proxy.data_handler_logic.get().value_data
+        final_times = self.data_handler_actor_proxy.data_handler_logic.get().time_data
+
+        assert final_data == [deserialise_f144(dat).value for dat in self.f144_data]
+        assert final_times == [deserialise_f144(dat).timestamp_unix_ns for dat in self.f144_data]
+
+
+class TestConsumerActorev44:
+    @pytest.fixture
+    def setup(self):
+        self.ev44_data = generate_fake_ev44_events(10)
+        self.consumer_stub = ConsumerStub({})
+        for i, data in enumerate(self.ev44_data):
+            self.consumer_stub.add_message('topic', 0, i+1, None, data)
+
+        self.data_handler_supervisor_mock = MagicMock()
+        self.interpolator_actor_mock = MagicMock(spec=InterpolatorActor)
+        self.interpolator_actor_mock.actor_ref.tell = MagicMock()
+        self.data_handler_actor_ref = DataHandlerActor.start(
+            self.data_handler_supervisor_mock,
+            self.interpolator_actor_mock,
+            DataHandlerLogic())
+        self.data_handler_actor_proxy = self.data_handler_actor_ref.proxy()
+
+        self.consumer_supervisor_mock = MagicMock()
+        self.consumer_logic = ConsumerLogic(self.consumer_stub)
+        self.consumer_actor_ref = ConsumerActor.start(self.consumer_supervisor_mock, self.data_handler_actor_ref, self.consumer_logic)
+        self.consumer_actor_proxy = self.consumer_actor_ref.proxy()
+        yield
+        ActorRegistry.stop_all()
+
+    def test_consumer_actor(self, setup):
+        # Check DataHandlerActor's state
+        assert self.data_handler_actor_proxy.get_status().get() == 'RUNNING'
+
+        self.consumer_actor_ref.tell('START')
+
+        assert self.consumer_actor_proxy.get_status().get() == 'RUNNING'
+
         time.sleep(0.1)
 
         self.consumer_actor_ref.tell('STOP')
 
         # Check the first message, it should be processed by the ev44 schema
         final_data = self.data_handler_actor_proxy.data_handler_logic.get().value_data
+        final_times = self.data_handler_actor_proxy.data_handler_logic.get().time_data
 
-        assert final_data == [deserialise_f144(dat).value for dat in self.f144_data]
+        assert final_data == [len(deserialise_ev44(dat).time_of_flight) for dat in self.ev44_data]
+        assert final_times == [deserialise_ev44(dat).time_of_flight[0] + deserialise_ev44(dat).reference_time for dat in self.ev44_data]
